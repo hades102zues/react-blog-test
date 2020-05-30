76fbd7bf74051f049c7815ff0d68908f1db349d3
Joshua Wiggins <joshua_kar@hotmail.com>
2020-05-29 20:43:28 -0400

    Logic is in and working! Styling is solved and working!

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Home.module.css";
 import PageDial from "./PageDial/PageDial";
 
-const home: React.FC = () => {
+interface State {
+  page: number;
+  total: number;
+  display: number;
+}
+
+const Home: React.FC = () => {
   const page: number = 6;
   const total: number = 1000;
-  const display: number = 9;
+  const display: number = 7;
 
+  const [state, setState] = useState({ page: 6, total: 1000, display: 7 }); //useState triggers a double render. put things u dont want acted upon twice in useEffect
+
+  const pageDialClickHandler = (nextPage: number): void => {
+    //function that sets the current page number
+    setState({
+      page: nextPage,
+      total: state.total,
+      display: state.display,
+    } as State);
+  };
+
+  console.log("Home: ", state);
   return (
     <div className={styles.home}>
       <PageDial
-        currentPage={page}
-        totalPages={total}
-        displayTotal={display} //Either 9, 7. 5 is fine only for mobile
-        color="#FF5722"
-        onClick={() => null}
+        currentPage={state.page}
+        totalPages={state.total}
+        onClick={pageDialClickHandler}
       />
     </div>
   );
 };
 
-export default home;
+export default Home;
 
 /*
   Invalid current page to total pages
--- src/pages/Home/PageDial/PageDial.tsx
+++ src/pages/Home/PageDial/PageDial.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import styles from "./PageDial.module.css";
-import { cpus } from "os";
 
 interface Props {
-  currentPage: number;
-  totalPages: number;
-  displayTotal: number;
-  onClick: (num: number) => void;
+  currentPage: number; //Current page no.
+  totalPages: number; //Total number of available pages
 
-  color?: string; //use this  to change the color of the component
+  onClick: (num: number) => void; //a function that will take the number that was pressed and update state above.
+
+  displayTotal?: number; //Use either 9 or 7 only! Defaults to 9.
+  color?: string; //use this  to change the color of the component. Defaults to purple.
 }
 
-const dial: React.FC<Props> = (props) => {
-  const { currentPage, totalPages, displayTotal, color } = props;
+const Dial: React.FC<Props> = (props) => {
+  // const { currentPage, totalPages, color, onClick } = props;
+  //let { displayTotal } = props;
+
+  const displayTotal =
+    props.displayTotal !== undefined ? props.displayTotal : 9;
+  const itemColor = props.color !== undefined ? props.color : "#7b1fa2";
 
   const pageDial: string[] = [];
   const sidePush = (displayTotal - 1) / 2;
@@ -20,7 +25,7 @@ const dial: React.FC<Props> = (props) => {
   /**
    * current page = 2
    */
-  pageDial.push(currentPage.toString());
+  pageDial.push(props.currentPage.toString());
 
   /*
     Attemp to push sidepush amount of items to the left side of current page
@@ -43,7 +48,7 @@ const dial: React.FC<Props> = (props) => {
   for (let i: number = 1; i <= sidePush; i++) {
     const itemAtLastIndex: number = Number(pageDial[pageDial.length - 1]);
 
-    if (itemAtLastIndex + 1 <= totalPages) {
+    if (itemAtLastIndex + 1 <= props.totalPages) {
       const item: string = (itemAtLastIndex + 1).toString();
 
       pageDial.push(item);
@@ -51,13 +56,13 @@ const dial: React.FC<Props> = (props) => {
   }
 
   //Attemp to fill unused item spaces
-  if (pageDial.length !== displayTotal) {
+  if (pageDial.length !== props.displayTotal) {
     //determines how many spaces to the left of the current page was not filled
-    const left: number = sidePush - (currentPage - Number(pageDial[0]));
+    const left: number = sidePush - (props.currentPage - Number(pageDial[0]));
 
     //determines how many spaces to the right of the current page was not filled
     const right: number =
-      sidePush - (Number(pageDial[pageDial.length - 1]) - currentPage);
+      sidePush - (Number(pageDial[pageDial.length - 1]) - props.currentPage);
 
     if (left > right) {
       //alot more missing to the left
@@ -67,7 +72,7 @@ const dial: React.FC<Props> = (props) => {
       for (let i: number = 1; i <= left; i++) {
         const itemAtLastIndex: number = Number(pageDial[pageDial.length - 1]);
 
-        if (itemAtLastIndex + 1 <= totalPages) {
+        if (itemAtLastIndex + 1 <= props.totalPages) {
           const item: string = (itemAtLastIndex + 1).toString();
 
           pageDial.push(item);
@@ -103,35 +108,55 @@ const dial: React.FC<Props> = (props) => {
       pageDial.unshift("1");
     }
 
-    if (Number(pageDial[pageDial.length - 1]) !== totalPages) {
+    if (Number(pageDial[pageDial.length - 1]) !== props.totalPages) {
       pageDial.pop();
       pageDial.pop();
       pageDial.push("...");
-      pageDial.push(totalPages.toString());
+      pageDial.push(props.totalPages.toString());
     }
   }
 
   pageDial.unshift("<<");
   pageDial.push(">>");
 
-  console.log("Current Page: ", currentPage);
-  console.log("Total Item view: ", displayTotal);
-  console.log(pageDial);
-  console.log("Total Number of Pages", totalPages);
+  // console.log("Current Page: ", props.currentPage);
+  // console.log("Total Item view: ", props.displayTotal);
+  // console.log(pageDial);
+  // console.log("Total Number of Pages", props.totalPages);
 
-  const itemColor = color !== undefined ? color : "#7b1fa2";
+  const onDialClickHandler = (
+    key: string,
+    current_page: number,
+    total_pages: number
+  ) => {
+    if (key.includes("...")) {
+      return;
+    } else if (key.includes(">>")) {
+      if (current_page + 1 <= total_pages && current_page !== total_pages)
+        props.onClick(current_page + 1);
+      return;
+    } else if (key.includes("<<")) {
+      if (current_page - 1 > 0) props.onClick(current_page - 1);
+      return;
+    } else {
+      props.onClick(Number(key));
+    }
+  };
 
   const dialItems: JSX.Element[] = pageDial.map((item, i) => {
     // const active: String =
     //   Number(pageDial[i]) === currentPage ? " " + styles.dial__item_active : "";
+    let active: boolean;
 
-    const active: boolean = pageDial[i].includes(currentPage.toString());
-
-    // const lastItemBorderFix = pageDial[i].includes(
-    //   pageDial[pageDial.length - 1]
-    // )
-    //   ? "1px solid #ccc"
-    //   : "";
+    if (
+      pageDial[i].includes("...") ||
+      pageDial[i].includes(">>") ||
+      pageDial[i].includes("<<")
+    )
+      active = false;
+    else {
+      active = Number(pageDial[i]) === props.currentPage ? true : false;
+    }
 
     return (
       <li
@@ -145,18 +170,32 @@ const dial: React.FC<Props> = (props) => {
           // color: active ? "#ffffff" : itemColor,
           // background: active ? itemColor : "",
         }}
+        onClick={(): void => {
+          onDialClickHandler(pageDial[i], props.currentPage, props.totalPages);
+        }}
       >
         <p
           onMouseEnter={(e: React.MouseEvent<HTMLElement>) => {
             (e.target as HTMLElement).style.background = itemColor;
+            (e.target as HTMLElement).style.color = "#ffffff";
           }}
-          onMouseLeave={(
-            e: React.MouseEvent<HTMLElement>,
-            isCurrentPage = active
-          ) => {
+          onMouseLeave={(e: React.MouseEvent<HTMLElement>) => {
             if (!active) {
               (e.target as HTMLElement).style.background = "";
+              (e.target as HTMLElement).style.color = itemColor;
+            } else {
+              (e.target as HTMLElement).style.color = "#ffffff";
+              (e.target as HTMLElement).style.background = itemColor;
             }
+          }}
+          onClick={(e: React.MouseEvent<HTMLElement>) => {
+            onDialClickHandler(
+              pageDial[i],
+              props.currentPage,
+              props.totalPages
+            );
+            if (!active) (e.target as HTMLElement).style.color = "#ffffff";
+            //(e.target as HTMLElement).style.background = "";
           }}
           className={styles.dial__item_active_corrector}
           style={{
@@ -170,15 +209,13 @@ const dial: React.FC<Props> = (props) => {
   });
 
   return (
-    <div className={styles.wrapper}>
-      <ul
-        className={styles.dial}
-        // style={{ width: ((displayTotal * 47.5) / 10).toString() + "rem" }}
-      >
-        {dialItems}
-      </ul>
-    </div>
+    <ul
+      className={styles.dial}
+      // style={{ width: ((displayTotal * 47.5) / 10).toString() + "rem" }}
+    >
+      {dialItems}
+    </ul>
   );
 };
 
-export default dial;
+export default Dial;
